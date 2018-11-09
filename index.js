f70a99a12cc9ba07d89790f01fa5c2d70bc7a5d5
Maria Jaramillo <mariajaramillogv@gmail.com>
2018-11-09 21:56:31 +0400

    finished

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const verificationController = require("./controllers/verification");
-app.get("/", verificationController);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3000, () => console.log("Webhook server is listening, port 3000"));
+
+
+const verificationController = require("./controllers/verification");
+app.get("/", verificationController);
+const messageWebhookController = require("./controllers/messageWebhook");
+app.post("/", messageWebhookController);
+
+
